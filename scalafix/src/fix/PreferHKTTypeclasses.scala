148b7421f5bb04a6cb4ce156d10d0c498643d302
MercurieVV <mercurievv@gmail.com>
2026-07-26 16:15:48 +0300

    Repair prePush failure for task #93

--- scalafix/src/fix/PreferHKTTypeclasses.scala
+++ scalafix/src/fix/PreferHKTTypeclasses.scala
@@ -1,7 +1,6 @@
 package fix
 
 import scalafix.v1._
-import scala.meta._
 
 final class PreferHKTTypeclasses extends SemanticRule("PreferHKTTypeclasses") {
   override def fix(implicit doc: SemanticDocument): Patch = Patch.empty
