c29a779ed8e7f7688040b59891ca29c5b37110c5
MercurieVV <mercurievv@gmail.com>
2026-07-26 15:17:53 +0300

    Repair prePush failure for task #93

--- scalafix/testOutput/src/golden/AbstractAlternativeEmptyAndChoice.scala
+++ scalafix/testOutput/src/golden/AbstractAlternativeEmptyAndChoice.scala
@@ -4,7 +4,7 @@ rules = [PreferHKTTypeclasses]
 package golden
 
 import cats.Alternative
-import cats.syntax.alternative._
+import cats.syntax.semigroupk._
 
 object AbstractAlternativeEmptyAndChoice {
   private def choose[G[_]: Alternative](x: G[Int], y: G[Int]): G[Int] = x <+> y
