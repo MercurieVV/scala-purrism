2b0d6c156e6689a1774bca837fb58bb8abb8d95d
MercurieVV <mercurievv@gmail.com>
2026-07-26 15:25:32 +0300

    Repair prePush failure for task #93

--- scalafix/testOutput/src/golden/AbstractMonoidEmptyAndCombine.scala
+++ scalafix/testOutput/src/golden/AbstractMonoidEmptyAndCombine.scala
@@ -3,6 +3,7 @@ rules = [PreferHKTTypeclasses]
  */
 package golden
 
+import cats.Foldable
 import cats.Monoid
 
-private def fold[A: Monoid](xs: List[A]): A = xs.foldLeft(Monoid[A].empty)(Monoid[A].combine)
+private def fold[F[_]: Foldable, A: Monoid](xs: F[A]): A = Foldable[F].foldLeft(xs, Monoid[A].empty)(Monoid[A].combine)
