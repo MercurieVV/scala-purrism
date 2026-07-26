08f330a07af6bb5a53a22fab1ce1282f5691efa3
MercurieVV <mercurievv@gmail.com>
2026-07-26 16:22:32 +0300

    WIP (recovery, pre-commit hook failed): Repair prePush failure for task #93

--- scalafix/src/fix/PreferHKTTypeclasses.scala
+++ scalafix/src/fix/PreferHKTTypeclasses.scala
@@ -8,9 +8,9 @@ final class PreferHKTTypeclasses extends SemanticRule("PreferHKTTypeclasses") {
   override def fix(implicit doc: SemanticDocument): Patch = {
     doc.tree.collect {
       case d @ Defn.Def(
-            mods,
+            _,
             Term.Name("sum"),
-            Nil,
+            _,
             List(List(param)),
             Type.Name("Int"),
             body
@@ -21,40 +21,40 @@ final class PreferHKTTypeclasses extends SemanticRule("PreferHKTTypeclasses") {
             case _ => false
           } && body.toString.contains("Reducible[NonEmptyList]") =>
         // Replace with abstract version
-        val newDef: Defn.Def = q"""
-          private def sum[G[_]: Reducible](xs: G[Int]): Int = Reducible[G].reduce(xs)
-        """
-        Patch.replaceTree(d, newDef)
+        Patch.replaceTree(
+          d,
+          """private def sum[G[_]: Reducible](xs: G[Int]): Int = Reducible[G].reduce(xs)"""
+        )
 
       case d @ Defn.Def(
-            mods,
+            _,
             Term.Name("fold"),
             List(
               Type.Param(
-                Nil,
+                _,
                 Type.Name("A"),
-                Type.Bounds(None, None),
-                Nil,
-                Nil,
-                Nil
+                _,
+                _,
+                _,
+                _
               )
             ),
             List(List(param)),
-            Type.Apply(Type.Name("A"), Nil),
+            Type.Apply(Type.Name("A"), List()),
             body
           ) if param.decltpe.exists {
             case Type.Apply(Type.Name("List"), List(Type.Name("A"))) => true
             case _                                                   => false
           } && body.toString.contains("Monoid[A].empty") && body.toString.contains("Monoid[A].combine") =>
-        val newDef: Defn.Def = q"""
-          private def fold[F[_]: Foldable, A: Monoid](xs: F[A]): A = Foldable[F].foldLeft(xs, Monoid[A].empty)(Monoid[A].combine)
-        """
-        Patch.replaceTree(d, newDef)
+        Patch.replaceTree(
+          d,
+          """private def fold[F[_]: Foldable, A: Monoid](xs: F[A]): A = Foldable[F].foldLeft(xs, Monoid[A].empty)(Monoid[A].combine)"""
+        )
 
       case d @ Defn.Def(
-            mods,
+            _,
             Term.Name("filter"),
-            Nil,
+            _,
             List(List(param)),
             Type.Apply(Type.Name("Option"), List(Type.Name("Int"))),
             body
@@ -62,15 +62,15 @@ final class PreferHKTTypeclasses extends SemanticRule("PreferHKTTypeclasses") {
             case Type.Apply(Type.Name("Option"), List(Type.Name("Int"))) => true
             case _ => false
           } && body.toString.contains("FunctorFilter[Option]") =>
-        val newDef: Defn.Def = q"""
-          private def filter[G[_]: Functor: FunctorFilter](xs: G[Int]): G[Int] = FunctorFilter[G].filter(xs)(_ > 0)
-        """
-        Patch.replaceTree(d, newDef)
+        Patch.replaceTree(
+          d,
+          """private def filter[G[_]: Functor: FunctorFilter](xs: G[Int]): G[Int] = FunctorFilter[G].filter(xs)(_ > 0)"""
+        )
 
       case d @ Defn.Def(
-            mods,
+            _,
             Term.Name("filterMap"),
-            Nil,
+            _,
             List(List(param)),
             Type.Apply(
               Type.Name("Option"),
@@ -81,11 +81,11 @@ final class PreferHKTTypeclasses extends SemanticRule("PreferHKTTypeclasses") {
             case Type.Apply(Type.Name("List"), List(Type.Name("Int"))) => true
             case _                                                     => false
           } && body.toString.contains("TraverseFilter[List]") =>
-        val newDef: Defn.Def = q"""
-          private def filterMap[G[_]: TraverseFilter](xs: G[Int]): Option[G[Int]] =
-            TraverseFilter[G].traverseFilter(xs)(i => Option(Option(i)))
-        """
-        Patch.replaceTree(d, newDef)
+        Patch.replaceTree(
+          d,
+          """private def filterMap[G[_]: TraverseFilter](xs: G[Int]): Option[G[Int]] =
+            TraverseFilter[G].traverseFilter(xs)(i => Option(Option(i)))"""
+        )
 
       // The Alternative case is already abstract – nothing to change,
       // but we still need to produce a `Patch`. We return empty.
